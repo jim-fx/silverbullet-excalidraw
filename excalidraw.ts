be51adc654820a61fc796f6ddcb99e5437449012
Max Richter <jim-x@web.de>
2025-04-11 10:12:19 +0200

    wip

--- excalidraw.ts
+++ excalidraw.ts
@@ -1,6 +1,25 @@
+import { asset } from "@silverbulletmd/silverbullet/syscalls";
+
+const env = "development";
+
+export async function viewer(): Promise<{ html: string; script: string }> {
+  const reactDom = await asset.readAsset("excalidraw", `assets/react-dom.${env}.js`);
+  const react = await asset.readAsset("excalidraw", `assets/react.${env}.js`);
+  const main = await asset.readAsset("excalidraw", "assets/main.js");
+  return {
+    html: `<link rel="stylesheet" href="https://unpkg.com/excalidraw@0.6.4/dist/excalidraw.min.css">
+<script src="https://unpkg.com/@excalidraw/excalidraw@0.18.0/dist/prod/index.js"></script>
+<div id="app"></div>`,
+    script: `${react}
+    ${reactDom}
+    ${main}`
+  }
+}
+
 export function widget(
   bodyText: string,
 ): { html: string; script: string } {
+  console.log({ bodyText })
   return {
     html: `
       <style>
@@ -11,20 +30,22 @@ export function widget(
           display: none !important;
        }
       </style>
+      <code style="display: none;">${bodyText}</code>
       <div id="app" style="min-height: 500px; overflow: hidden; border-radius: 5px;"/>
     `,
     script: `
       function onChange(elements, state) {
         sessionStorage.setItem("excalidraw-state", JSON.stringify(state));
         sessionStorage.setItem("excalidraw-elements", JSON.stringify(elements));
+        console.log({elements})
       }
+
+      const elements = JSON.parse(document.getElementsByTagName("code")[0]?.innerText||sessionStorage.getItem("excalidraw-elements")||'[]');
 
       const App = () => {
         const appState = JSON.parse(sessionStorage.getItem("excalidraw-state")||"{}");
         delete appState.collaborators;
         const theme = appState?.theme || "light";
-  
-        const elements = JSON.parse(sessionStorage.getItem("excalidraw-elements")||"[]");
 
         return React.createElement(
           React.Fragment,
@@ -43,19 +64,6 @@ export function widget(
         );
       };
 
-      Promise.all([
-        loadJsByUrl("https://unpkg.com/@excalidraw/excalidraw/dist/excalidraw.development.js"),
-        loadJsByUrl("https://unpkg.com/react@18.2.0/umd/react.development.js"),
-        loadJsByUrl("https://unpkg.com/react-dom@18.2.0/umd/react-dom.development.js")
-      ]).then(() => {
-        const excalidrawWrapper = document.getElementById("app");
-        const root = ReactDOM.createRoot(excalidrawWrapper);
-        root.render(React.createElement(App));
-        updateHeight();
-        setTimeout(() => {
-          updateHeight();
-        }, 1000);
-      });
     `,
   };
 }
